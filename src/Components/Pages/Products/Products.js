00343164c4be7d521943fe9e6904a08839e6668f
Nur-Alom <alamn7150@gmail.com>
2022-09-13 21:32:31 +0600

    second commit

--- src/Components/Pages/Products/Products.js
+++ src/Components/Pages/Products/Products.js
@@ -3,10 +3,12 @@ import Header from '../Header/Header';
 
 const Products = () => {
     return (
-        <div>
+        <div className='flex flex-col flex-1 w-full h-screen'>
             <Header />
-            <div>
-                <h2>Hello From Products</h2>
+            <div className='h-full overflow-y-auto'>
+                <div className='px-6 mx-auto'>
+                    <h2>Hello From Products</h2>
+                </div>
             </div>
         </div>
     );
